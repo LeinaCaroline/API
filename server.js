ae08b80876e7f17ae66b39941024408fc10b77aa
Leina <leina.caroline@gmail.com>
2024-08-27 16:34:26 -0300

    Acrescentei a requisição POST na API

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+const users = []
+
+
+app.post('/usuarios', (req,res) =>{
+    console.log = (req.boby)
+    res.send('Ok deu certo')
+})
+
+
 
 app.get('/usuarios', (req,res) =>{
     res.send('Ok, deu bom')
 })
 
-app.listen(3000)
+    app.listen(3000)
